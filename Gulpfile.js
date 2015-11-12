5709bb583c27aeb98e2f7e5af0b5043c153e1be4
Rajan Lamichhane <rajanlamic@gmail.com>
2015-11-12 15:45:19 +0000

    first commit

---
+++ Gulpfile.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Rajan on 11/11/2015.
+ */
+
+var gulp = require("gulp");
+var sourcemaps = require("gulp-sourcemaps");
+var babel = require("gulp-babel");
+var concat = require("gulp-concat");
+var browserify = require("gulp-browserify");
+
+gulp.task("build", function () {
+    return gulp.src("src/**/*.js")
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            "presets": ["es2015"]
+        }))
+        .pipe(concat("all.js"))
+        .pipe(browserify(
+            {
+                debug: true
+            }
+        ))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task('watch', ['build'], function () {
+    gulp.watch('src/*.js', ['build']);
+});
+
+gulp.task('default', ['watch']);
---
+++ src/first.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Rajan on 10/11/2015.
+ */
+
+"use strict";
+
+class GetMe {
+
+    alert() {
+        alert('alert action');
+    }
+    log() {
+        console.log('log ed');
+    }
+}
+
+module.exports = GetMe;
